4e261d90e6d909e222ec921c8639dff48e78e179
hy3oning <shyun5635@gmail.com>
2026-02-02 17:28:55 +0900

    [update] Aspect After JoingPoint Test

--- src/main/java/com/zeus/common/aop/ServiceLoggerAdvice.java
+++ src/main/java/com/zeus/common/aop/ServiceLoggerAdvice.java
@@ -3,6 +3,7 @@ package com.zeus.common.aop;
 import java.util.Arrays;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -19,5 +20,11 @@ public class ServiceLoggerAdvice {
 		log.info("Aspect beforeLog");
 		log.info("Aspect beforeLog jp = "+ jp.getSignature());
 		log.info("Aspect beforeLog jp = "+ Arrays.toString(jp.getArgs()));
+	}
+	@After("execution(* com.zeus.service.ItemService*.*(..))")
+	public void afterLog(JoinPoint jp) {
+		log.info("Aspect afterLog");
+		log.info("Aspect afterLog jp = "+ jp.getSignature());
+		log.info("Aspect afterLog jp = "+ Arrays.toString(jp.getArgs()));
 	}
 }
