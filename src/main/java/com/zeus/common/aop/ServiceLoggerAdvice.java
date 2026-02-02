f9738972a0b871b4d4a53ce4c26bbd54d6baad50
hy3oning <shyun5635@gmail.com>
2026-02-02 17:24:43 +0900

    [update] before JoingPoing Test

--- src/main/java/com/zeus/common/aop/ServiceLoggerAdvice.java
+++ src/main/java/com/zeus/common/aop/ServiceLoggerAdvice.java
@@ -1,5 +1,23 @@
 package com.zeus.common.aop;
 
+import java.util.Arrays;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@Aspect
 public class ServiceLoggerAdvice {
 
+	@Before("execution(* com.zeus.service.ItemService*.*(..))")
+	public void beforeLog(JoinPoint jp) {
+		log.info("Aspect beforeLog");
+		log.info("Aspect beforeLog jp = "+ jp.getSignature());
+		log.info("Aspect beforeLog jp = "+ Arrays.toString(jp.getArgs()));
+	}
 }
